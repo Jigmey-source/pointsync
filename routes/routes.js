a366ec82c13705611a783602ce2a292e21a6d118
Jigmey-source <76196934+Jigmey-source@users.noreply.github.com>
2024-02-17 16:07:41 +0530

    a

--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,12 @@ import feeds from '../models/feeds.js';
 const routes = new Route();
 
 
+router.get('/', async function (req, res) {
+    const response = { message: "start this party" }
+    res.json(response);
+    console.log('muahhahahahahhahahaahhhash');
+});
+
 router.post(routes.signUp, async function (req, res) {
     const muser = new user({
         group: req.body.group,
