b791e2e788267ed01990ad938a6d9889ed794b71
Jigmey-source <76196934+Jigmey-source@users.noreply.github.com>
2024-02-17 18:23:49 +0530

    a

--- server.js
+++ server.js
@@ -24,34 +24,64 @@ import notify from './routes/notify.js';
 import deleteuser from './routes/delete.js';
 
 const serviceAccount = "./pointsync-5d5bf-firebase-adminsdk-sta42-140fb8a7fc.json";
+const mongodb = "mongodb+srv://jigmey:fr33t1b3t@cluster0.bsbuyml.mongodb.net/Pointsync?retryWrites=true&w=majority";
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: "pointsync-5d5bf.appspot.com",
 });
 
-try {
-    connect('mongodb+srv://jigmey:fr33t1b3t@cluster0.bsbuyml.mongodb.net/Pointsync?retryWrites=true&w=majority');
-} catch (e) {
-    console.log(e);
-} finally {
-    app.use('/', routes);
-    app.use('/post', post);
-    app.use('/record', record);
-    app.use('/user', user);
-    app.use('/edit', edit);
-    app.use('/locality', locality);
-    app.use('/store', store);
-    app.use('/search', search);
-    app.use('/notify', notify);
-    app.use('/delete', deleteuser);
-
-    app.use('/', (req, res) => {
-        const response = { message: "start this party now" }
-        console.log('muahhahahahahhahahaahhhash');
-        res.json(response);
-    });
+const connectmongodb = async () => {
+    try {
+        const conn = await connect(mongodb);
+        console.log(`MONGODB connection secured : ${conn.connection.host}`);
+    } catch (e) { 
+        console.log(`There seems to be an error connecting to mongodb : ${error}`);
+        process.exit(1);
+    }
 };
+
+connectmongodb();
+
+app.use('/', routes);
+app.use('/post', post);
+app.use('/record', record);
+app.use('/user', user);
+app.use('/edit', edit);
+app.use('/locality', locality);
+app.use('/store', store);
+app.use('/search', search);
+app.use('/notify', notify);
+app.use('/delete', deleteuser);
+
+app.use('/', (req, res) => {
+    const response = { message: "start this party now" }
+    console.log('muahhahahahahhahahaahhhash');
+    res.json(response);
+});
+
+// try {
+//     connect(mongodb);
+// } catch (e) {
+//     console.log(e);
+// } finally {
+//     app.use('/', routes);
+//     app.use('/post', post);
+//     app.use('/record', record);
+//     app.use('/user', user);
+//     app.use('/edit', edit);
+//     app.use('/locality', locality);
+//     app.use('/store', store);
+//     app.use('/search', search);
+//     app.use('/notify', notify);
+//     app.use('/delete', deleteuser);
+
+//     app.use('/', (req, res) => {
+//         const response = { message: "start this party now" }
+//         console.log('muahhahahahahhahahaahhhash');
+//         res.json(response);
+//     });
+// };
 
 const port = process.env.PORT || 5500;
 const server = app.listen(port, function () {
