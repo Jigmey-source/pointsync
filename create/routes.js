1bf8b0b4aca1f202cf046484bd2ebc204a0b1a73
Jigmey-source <76196934+Jigmey-source@users.noreply.github.com>
2024-03-05 16:12:43 +0530

    a

--- create/routes.js
+++ create/routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import user from "../models/user.js";
 import {
     comment, icon, inventory, like, notification,
     recommend, report, review, snapshot, store, account
@@ -26,6 +27,25 @@ router.post('/inventory', inventory);
 
 router.post('/store', store);
 
-router.post('/account', account);
+// router.post('/account', account);
+router.post('/account', async (req, res) => {
+    const muser = new user({
+        group: req.body.group,
+        placemarker: req.body.placemarker,
+        name: req.body.name,
+        token: req.body.token,
+        userId: req.body.userId,
+    });
+    console.log('inside createuser method registration aaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    try {
+        await muser.save();
+        const response = { message: "New User has been created" };
+        res.json(response);
+    } catch (e) {
+        console.log('ERROR: creating new user ' + e);
+        res.json(e);
+    }
+    console.log('after createuser try catch function ddddddddddddddddddddddd');
+});
 
 export default router;
