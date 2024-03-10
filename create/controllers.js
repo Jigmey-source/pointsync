f06b5347eca9493de899d0f86114916c8950e0dc
Jigmey-source <76196934+Jigmey-source@users.noreply.github.com>
2024-03-10 18:42:03 +0530

    a

--- create/controllers.js
+++ create/controllers.js
@@ -162,23 +162,3 @@ export const store = async (req, res) => {
         res.json(e);
     }
 }
-
-// export const account = async (req, res) => {
-//     const muser = new user({
-//         group: req.body.group,
-//         placemarker: req.body.placemarker,
-//         name: req.body.name,
-//         token: req.body.token,
-//         userId: req.body.userId,
-//     });
-//     console.log('inside createuser method registration aaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-//     try {
-//         await muser.save();
-//         const response = { message: "New User has been created" };
-//         res.json(response);
-//     } catch (e) {
-//         console.log('ERROR: creating new user ' + e);
-//         res.json(e);
-//     }
-//     console.log('after createuser try catch function ddddddddddddddddddddddd');
-// }
